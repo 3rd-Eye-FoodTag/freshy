0f5acd93579be7372ce367abb2bfa926b73f837a
thyang0119 <th.yang.0119@gmail.com>
2024-09-08 09:18:33 -0700

    tianhao-add-new-item-modal

--- src/component/CenterMenu/index.tsx
+++ src/component/CenterMenu/index.tsx
@@ -1,34 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, TouchableOpacity, Text, Modal} from 'react-native';
+import ManualInputModal from '../ManualInputModal';
 
 const CenterMenu = ({isMenuVisible, setMenuVisible}) => {
+  const [isSearchModalVisible, setSearchModalVisible] = useState(false);
   const toggleMenu = () => {
+    setMenuVisible(!isMenuVisible);
+  };
+
+  const handleManualInput = () => {
+    console.log('Manual Input selected');
+    setSearchModalVisible(true);
+    setMenuVisible(!isMenuVisible);
+  };
+
+  const handleBarcode = () => {
+    console.log('Barcode selected');
+    setMenuVisible(!isMenuVisible);
+  };
+
+  const handleSmartCameraRecognition = () => {
+    console.log('Smart Camera Recognition selected');
+    setMenuVisible(!isMenuVisible);
+  };
+
+  const handleReceipt = () => {
+    console.log('Receipt selected');
+    setMenuVisible(!isMenuVisible);
+  };
+
+  const handleCreateNewItem = () => {
+    console.log('Create a new item selected');
     setMenuVisible(!isMenuVisible);
   };
 
   return (
     <View style={styles.container}>
-      {/* Overlay for graying out the background */}
-      <Modal transparent animationType="fade" visible={isMenuVisible}>
-        <TouchableOpacity style={styles.overlay} onPress={toggleMenu} />
-        <View style={styles.menuContainer}>
-          <TouchableOpacity style={styles.menuButton}>
-            <Text style={styles.menuText}>Manual Input</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.menuButton}>
-            <Text style={styles.menuText}>Barcode</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.menuButton}>
-            <Text style={styles.menuText}>Smart Camera Recognition</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.menuButton}>
-            <Text style={styles.menuText}>Receipt</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.menuButton}>
-            <Text style={styles.menuText}>Create a new item</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
+      {isMenuVisible && (
+        <Modal transparent animationType="fade" visible={isMenuVisible}>
+          <TouchableOpacity style={styles.overlay} onPress={toggleMenu} />
+          <View style={styles.menuContainer}>
+            <TouchableOpacity
+              style={styles.menuButton}
+              onPress={handleManualInput}>
+              <Text style={styles.menuText}>Manual Input</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuButton} onPress={handleBarcode}>
+              <Text style={styles.menuText}>Barcode</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.menuButton}
+              onPress={handleSmartCameraRecognition}>
+              <Text style={styles.menuText}>Smart Camera Recognition</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuButton} onPress={handleReceipt}>
+              <Text style={styles.menuText}>Receipt</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.menuButton}
+              onPress={handleCreateNewItem}>
+              <Text style={styles.menuText}>Create a new item</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      )}
+
+      <ManualInputModal
+        visible={isSearchModalVisible}
+        onClose={() => setSearchModalVisible(false)}
+      />
     </View>
   );
 };
@@ -59,23 +99,15 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
   },
-  customButton: {
-    position: 'absolute',
-    bottom: 20,
+  fab: {
     backgroundColor: '#4CAF50',
-    borderRadius: 35,
-    width: 70,
-    height: 70,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'center',
-    left: '50%',
-    transform: [{translateX: -35}],
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    elevation: 5,
+    position: 'absolute',
+    bottom: 30,
   },
   fabText: {
     color: '#fff',
